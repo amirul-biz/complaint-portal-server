ee3001670c2402571484a1c2b47000589994e1e4
amirul.azreem <amirul.irfan.biz@gmail.com>
2025-08-13 23:26:34 +0800

    main

--- dist/server.js
+++ dist/server.js
@@ -5,8 +5,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-    res.json({ message: "Complaint Portal API is running 🚀" });
+app.get("/", (_req, res) => {
+    res.json({ message: "Complaint Portal API is running" });
 });
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
 //# sourceMappingURL=server.js.map
